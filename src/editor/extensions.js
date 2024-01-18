446db2651e4dc6e8e9379037e255c7766850dc4c
ana-oprea <anaop3212@gmail.com>
2024-01-18 17:14:20 +0200

    fix: check if element exists when rendering extensions -refs #263143

--- src/editor/extensions.js
+++ src/editor/extensions.js
@@ -4,7 +4,7 @@ export const withGlossary = (editor) => {
   const { isInline } = editor;
 
   editor.isInline = (element) => {
-    return element.type === GLOSSARY ? true : isInline(element);
+    return element && element.type === GLOSSARY ? true : isInline(element);
   };
 
   return editor;
